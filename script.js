5fce8df3d4ef5e9ffa27058d202612d1f041568b
Roshan-Kumar-Mishra <144347812+Roshan-Kumar-Mishra@users.noreply.github.com>
2023-12-10 20:53:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 //your JS code here. If required.
-function functionName {
-	alert(arguments.callee.name);
+function printMyName() {
+  console.log(arguments.callee.name);
 }
+
+// Call the function
+printMyName();
